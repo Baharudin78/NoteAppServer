6914be8da0d8c1532562c09787b8578f82c0f6fd
Baharudin78 <bencoxgame78@gmail.com>
2021-10-07 07:01:33 +0700

    changes db

--- src/repository/DatabaseFactory.kt
+++ src/repository/DatabaseFactory.kt
@@ -4,11 +4,13 @@ import com.example.data.table.NoteTable
 import com.example.data.table.UserTable
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -23,10 +25,16 @@ object DatabaseFactory {
     fun hikari() : HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("DATABASE_URL")
+//        config.jdbcUrl = System.getenv("DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
         config.validate()
 
         return HikariDataSource(config)
